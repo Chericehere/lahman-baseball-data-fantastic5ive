8c166dfb18ee71046c4d8d3b196bc7ee0212de09
Cristian.Vargas9 <cristianvargas562@gmail.com>
2021-03-11 21:07:33 -0600

    working

--- data.sql
+++ data.sql
@@ -1,7 +1,7 @@
 Select *
 FROM teams;
 
-/*question 1*/
+--question 1--
 --1871-2017--
 SELECT MIN(debut), MAX(finalgame)
 FROM people;
@@ -17,7 +17,26 @@ ON t.teamid=b.teamid
 WHERE p.playerid='gaedeed01'
 ORDER BY height asc;
 
---Question 7,a--
+--QUESTION 3--
+
+SELECT   DISTINCT(p.playerid), p.namefirst as First_Name, p.namelast as Last_Name, Sum(r.salary) as majorsal
+FROM people as p
+LEFT JOIN salaries as r
+ON p.playerid = r.playerid
+WHERE p.playerid IN 
+(Select DISTINCT(playerid)
+	From collegeplaying as c
+	Left Join schools as s
+	ON c.schoolid = s.schoolid
+	WHERE s.schoolname = 'Vanderbilt University')
+Group by p.playerid
+Order by majorsal DESC;
+
+--David Price--
+
+
+
+--Question 7--
 SELECT teamid, yearid, w, WSWin
 FROM teams
 WHERE yearid > 1969
@@ -63,30 +82,59 @@ FROM 	winners LEFT JOIN max_wins
 		
 --12 wins; 26.09%--
 
-SELECT DISTINCT NL.playerid, CONCAT(p.namefirst, ' ', p.namelast) AS manager_name, nl.yearid AS year, 'NL' AS league
-FROM (
-	SELECT *
-	FROM awardsmanagers
-	WHERE lgid = 'NL' AND awardid = 'TSN Manager of the Year') AS NL
-INNER JOIN (
-	SELECT *
-	FROM awardsmanagers
-	WHERE lgid = 'AL' AND awardid = 'TSN Manager of the Year') AS AL
-USING (playerid)
-LEFT JOIN people AS p
-USING (playerid)
-UNION
-SELECT DISTINCT AL.playerid, CONCAT(p.namefirst, ' ', p.namelast) AS manager_name, AL.yearid AS year, 'AL' AS league
-FROM (
-	SELECT *
-	FROM awardsmanagers
-	WHERE lgid = 'NL' AND awardid = 'TSN Manager of the Year') AS NL
-INNER JOIN (
-	SELECT *
-	FROM awardsmanagers
-	WHERE lgid = 'AL' AND awardid = 'TSN Manager of the Year') AS AL
-USING (playerid)
-LEFT JOIN people AS p
-USING (playerid)
-ORDER BY league 
+--Question 8--
 
+SELECT park, team, games, attendance, ROUND((attendance/games),2) as avg_attendance
+FROM
+	(SELECT t.teamid as team_letters, t.park as park, t.name as team, 
+	 h.park as parkletters, CAST(h.games as numeric) as games, 
+	 CAST(h.attendance as numeric) as attendance
+	FROM teams as t
+	LEFT JOIN homegames as h
+	ON t.teamid = h.team
+	WHERE h.games > 10 AND h.year = 2016 AND t.yearid = 2016) as sub
+ORDER BY avg_attendance desc
+LIMIT 5;
+
+--TOP 5 Parks--
+
+
+
+--Question 9--
+
+SELECT DISTINCT NL.playerid, CONCAT(p.namefirst, ' ', p.namelast) AS manager_name, nl.yearid AS year, m.teamid, t.name, 'NL' AS league
+FROM (
+	SELECT *
+	FROM awardsmanagers
+	WHERE lgid = 'NL' AND awardid = 'TSN Manager of the Year') AS NL
+INNER JOIN (
+	SELECT *
+	FROM awardsmanagers
+	WHERE lgid = 'AL' AND awardid = 'TSN Manager of the Year') AS AL
+USING (playerid)
+LEFT JOIN people AS p
+USING (playerid)
+LEFT JOIN managers AS m
+ON nl.playerid = m.playerid AND nl.yearid = m.yearid
+LEFT JOIN teams AS t
+ON m.teamid = t.teamid AND nl.yearid = t.yearid
+UNION
+SELECT DISTINCT AL.playerid, CONCAT(p.namefirst, ' ', p.namelast) AS manager_name, AL.yearid AS year, m.teamid, t.name, 'AL' AS league
+FROM (
+	SELECT *
+	FROM awardsmanagers
+	WHERE lgid = 'NL' AND awardid = 'TSN Manager of the Year') AS NL
+INNER JOIN (
+	SELECT *
+	FROM awardsmanagers
+	WHERE lgid = 'AL' AND awardid = 'TSN Manager of the Year') AS AL
+USING (playerid)
+LEFT JOIN people AS p
+USING (playerid)
+LEFT JOIN managers AS m
+ON al.playerid = m.playerid AND al.yearid = m.yearid
+LEFT JOIN teams AS t
+ON m.teamid = t.teamid AND al.yearid = t.yearid
+ORDER BY league;
+
+--
